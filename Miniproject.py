99dabff751032bf269dd217ec982447d6ea95bf5
Aileus <vpratyush81@gmail.com>
2025-06-26 13:17:03 +0530

    movies id added

--- Miniproject.py
+++ Miniproject.py
@@ -4,42 +4,42 @@ def write():
     
     with open(r'Holiday Homework\miniproj.dat', 'wb') as f:
         data = [
-    {
-        "name": "Inception",
-        "production_cost": 160000000,
-        "director": "Christopher Nolan",
-        "duration": 148,
-        "budget": 165_000_000
-    },
-    {
-        "name": "The Shawshank Redemption",
-        "production_cost": 25000000,
-        "director": "Frank Darabont",
-        "duration": 142,
-        "budget": 28_000_000
-    },
-    {
-        "name": "The Godfather",
-        "production_cost": 6000000,
-        "director": "Francis Ford Coppola",
-        "duration": 175,
-        "budget": 6_500_000
-    },
-    {
-        "name": "Avengers: Endgame",
-        "production_cost": 356000000,
-        "director": "Anthony and Joe Russo",
-        "duration": 181,
-        "budget": 400_000_000
-    },
-    {
-        "name": "Parasite",
-        "production_cost": 11400000,
-        "director": "Bong Joon-ho",
-        "duration": 132,
-        "budget": 12_000_000
-    }
-]
+            {
+                "m_id": "M001",
+                "name": "Inception",
+                "production_cost": 160000000,
+                "director": "Christopher Nolan",
+                "duration": 148
+            },
+            {
+                "m_id": "M002",
+                "name": "The Shawshank Redemption",
+                "production_cost": 25000000,
+                "director": "Frank Darabont",
+                "duration": 142
+            },
+            {
+                "m_id": "M003",
+                "name": "The Godfather",
+                "production_cost": 6000000,
+                "director": "Francis Ford Coppola",
+                "duration": 175
+            },
+            {
+                "m_id": "M004",
+                "name": "Avengers: Endgame",
+                "production_cost": 356000000,
+                "director": "Anthony and Joe Russo",
+                "duration": 181
+            },
+            {
+                "m_id": "M005",
+                "name": "Parasite",
+                "production_cost": 11400000,
+                "director": "Bong Joon-ho",
+                "duration": 132
+            }
+        ]
         pickle.dump(data,f)
 write()        
         
@@ -47,6 +47,18 @@ def show():
     import pickle
     
     with open(r'Holiday Homework\miniproj.dat', 'rb') as f:
-        data = pickle.load(f)
-        print(data[0]['production_cost'])
+        movies = pickle.load(f)
+        
+        for movie in movies:
+            for k,v in movie.items():
+                
+                a = str(k)
+                a = a.capitalize()
+                if a == 'Budget' : a = 'Budget ($)'
+                if a == 'Duration' : a = 'Duratuon (mins)'
+                if a == 'Production_cost' : a = 'Production Cost ($)'
+                
+                print(a,":",v)
+            print('\n')
+            
 show()
